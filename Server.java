92758d04c2964d02024582a0caf484922b8e49c7
akash-d-dev <ak45hhere@gmail.com>
2024-09-06 11:27:55 +0530

    init client

---
+++ Server.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public void run() {
+        int port = 8010;
+        try (ServerSocket socket = new ServerSocket(port)) {
+            socket.setSoTimeout(10000);
+            while (true) {
+                System.err.println("Server is listening on " + port);
+                try {
+                    Socket acceptedConnection = socket.accept();
+                    System.out.println("Accepted connection from " + acceptedConnection.getRemoteSocketAddress());
+                    PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+                    toClient.println("Hello, client!");
+                    BufferedReader fromClient = new BufferedReader(
+                            new InputStreamReader(acceptedConnection.getInputStream()));
+                } catch (IOException e) {
+                    System.err.println("Error accepting connection: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.run();
+        } catch (Exception e) {
+            System.err.println("Server error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
